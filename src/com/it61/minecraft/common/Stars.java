be7c614fb0469646c0fe049b068c1d07f8f3973f
niuqy <niuqy@tedu.cn>
2017-08-11 17:29:22 +0800

    create constant class stars

---
+++ src/com/it61/minecraft/common/Stars.java
@@ -0,0 +1,19 @@
+package com.it61.minecraft.common;
+
+/**
+ *表示星座的常量类
+ */
+public class Stars {
+	public static final String BYZ = "白羊座";
+	public static final String JNZ = "金牛座";
+	public static final String SHUANGZZ = "双子座";
+	public static final String JXZ = "巨蟹座";
+	public static final String SHIZZ = "狮子座";
+	public static final String CNZ = "处女座";
+	public static final String TCZ = "天秤座";
+	public static final String TXZ = "天蝎座";
+	public static final String SSZ = "射手座";
+	public static final String MJZ = "摩羯座";
+	public static final String SPZ = "水瓶座";
+	public static final String SYZ = "双鱼座";
+}
