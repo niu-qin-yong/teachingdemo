7eb9fadaed3c26009732c4a97a1e1c24e179e78e
niuqy <niuqy@tedu.cn>
2017-08-11 17:04:07 +0800

    fix spell error

--- src/com/it61/minecraft/bean/User.java
+++ src/com/it61/minecraft/bean/User.java
@@ -2,15 +2,15 @@ package com.it61.minecraft.bean;
 
 public class User {
 	private int id;
-	private String username;
+	private String userName;
 	private String password;
-	private String nick_name;
+	private String nickName;
 	private String gender;
 	private int age;
 	private String banji;
-	private String phonenumbe;
+	private String phoneNumber;
 	private String star;
 	private String email;
 	private String grade;
-	private String experienc;
+	private String experience;
 }
