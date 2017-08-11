76fe861a6675d68789964d93520e7dd3430e9c98
niuqy <niuqy@tedu.cn>
2017-08-11 16:07:58 +0800

    1st lesson to create javabean User

---
+++ src/com/it61/minecraft/bean/User.java
@@ -0,0 +1,16 @@
+package com.it61.minecraft.bean;
+
+public class User {
+	private int id;
+	private String username;
+	private String password;
+	private String nick_name;
+	private String gender;
+	private int age;
+	private String banji;
+	private String phonenumbe;
+	private String star;
+	private String email;
+	private String grade;
+	private String experienc;
+}
